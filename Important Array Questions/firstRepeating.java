031f7965ed047d41d112d8a9052e5e0fe3fd7616
unknown <sanchitmishra88@gmail.com>
2020-06-17 11:02:27 +0530

    Array Question

---
+++ Important Array Questions/firstRepeating.java
@@ -0,0 +1,38 @@
+/*First Repeating element from an array using HashMap
+Store number of occurence and look for occurence greater than 1 in HashMap
+TC: O(N) & SC:O(N)*/
+
+import java.util.*;
+
+class firstRepeating{
+
+	static int repeating(int[] arr){
+		HashMap<Integer,Integer> hmap = new HashMap<Integer,Integer>();
+		for(int i=0; i < arr.length;i++){
+			if(hmap.containsKey(arr[i])){
+				hmap.put(arr[i],hmap.get(arr[i])+1);
+			}
+			else{
+				hmap.put(arr[i],1);
+			}
+		}
+		for(int i=0; i < hmap.size(); i++){
+			if(hmap.get(arr[i]) > 1){
+				return arr[i];
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int no;
+		Scanner sc = new Scanner(System.in);
+		no = sc.nextInt();
+		int[] arr = new int[no];
+		for(int i=0; i < no; i++){
+			arr[i] = sc.nextInt();
+		}
+		int first = repeating(arr);
+		System.out.println("First repeating Number is :" + first);
+	}
+}
